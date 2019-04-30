99877f74f4e0edd53412fa2e8d99b47efdb9ecf9
Victoria Guerrero <veeguerrero3@gmail.com>
2019-04-30 15:26:13 -0500

    add controller

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -1,13 +1,52 @@
-require "r00lz/version"
+require 'r00lz/version'
 
 module R00lz
   class Error < StandardError; end
+
+  # The Controller class
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
+    end
+  end
+
+  # The R00lz App
   class App
     def call(env)
+      kl, act = cont_and_act(env)
+      text = kl.new(env).send(act)
+
       [200,
-        { 'Content-Type' => 'text/html'},
-        ["hello, banana town!"]
-      ]
+       { 'Content-Type' => 'text/html' },
+       [text]]
     end
+
+    def cont_and_act(env)
+      _, con, act, after =
+        env['PATH_INFO'].split('/')
+
+      con = con.capitalize + 'Controller'
+
+      [Object.const_get(con), act]
+    end
+  end
+
+  def self.to_underscore(s)
+    s.gsub(
+      /([A-Z]+)([A-Z][a-z])/,
+      '\1_\2'
+    ).gsub(
+      /([a-z\d])([A-Z])/,
+      '\1_\2'
+    ).downcase
+  end
+end
+# Add object class method
+class Object
+  def self.const_missing(c)
+    require R00lz.to_underscore(c.to_s)
+    Object.const_get(c)
   end
 end
